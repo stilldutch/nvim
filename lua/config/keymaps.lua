e04490ae44882a8e2c1e9c9b4e3ba3857c77a6c9
ramons <ramons@intermax.nl>
2025-06-26 16:01:43 +0200

    Feat: lsp.lua update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end,
+    {desc="Open Diagnostic in Float"}
+)
